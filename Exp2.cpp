007bde62e5f24a00275382aad2d371e82774ece0
Moksh Gupta <115498284+mokshgupta03@users.noreply.github.com>
2024-02-01 11:27:14 +0530

    07i76iuk

--- Exp2.cpp
+++ Exp2.cpp
@@ -3,19 +3,19 @@ using namespace std;
 int main()
 {
     int i,a[10],n,ele,lb=0,ub,mid;
-cout<<"Enter the no of elements you want in array"<<endl;
+cout<<"Enter the no of elements you want in 1D matrix"<<endl;
 cin>>n;
-cout<<"Enter elements in the array "<<endl;
+cout<<"Enter elements in the 1D matrix "<<endl;
 for(i=0;i<n;i++)
 {
     cin>>a[i];
 }
-cout<<"The array is "<<endl;
+cout<<"The 1D matrix is "<<endl;
 for(i=0;i<n;i++)
 {
     cout<<a[i]<<" ";
 }
-cout<<"Enter the element you want to search in array "<<endl;
+cout<<"Enter the element you want to search in 1D matrix "<<endl;
 cin>>ele;
 ub=n-1;
 while(lb<=ub)
